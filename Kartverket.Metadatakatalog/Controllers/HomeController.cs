db2306a8c40a37cac1b3fad592f69c33150a44ed
Henning Jensen <henning@arkitektum.no>
2014-10-15 11:26:04 +0200

    Create empty ASP MVC 5 application

---
+++ Kartverket.Metadatakatalog/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Kartverket.Metadatakatalog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
