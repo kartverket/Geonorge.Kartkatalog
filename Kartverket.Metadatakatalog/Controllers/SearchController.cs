ab07d7701c905527221b37f487ce3b027445597b
Dag Olav <dagolav@arkitektum.no>
2014-10-31 13:14:35 +0100

    changed query for change in fields, added missing fields, created maps possibility

--- Kartverket.Metadatakatalog/Controllers/SearchController.cs
+++ Kartverket.Metadatakatalog/Controllers/SearchController.cs
@@ -29,16 +29,9 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
                      || Query.Field("abstract").Is(qstr).Boost(3)
                      || Query.Field("purpose").Is(qstr)
                      || Query.Field("type").Is(qstr)
+                     || Query.Field("theme").Is(qstr)
+                     || Query.Field("organization").Is(qstr)
                      || Query.Field("topic_category").Is(qstr)
-                     || Query.Field("contact_metadata_name").Is(qstr)
-                     || Query.Field("contact_metadata_organization").Is(qstr)
-                     || Query.Field("contact_metadata_email").Is(qstr)
-                     || Query.Field("contact_owner_name").Is(qstr)
-                     || Query.Field("contact_owner_organization").Is(qstr)
-                     || Query.Field("contact_owner_email").Is(qstr)
-                     || Query.Field("contact_publisher_name").Is(qstr)
-                     || Query.Field("contact_publisher_organization").Is(qstr)
-                     || Query.Field("contact_publisher_email").Is(qstr)
                      || Query.Field("keyword").Is(qstr)
                      ;
                  return query;
@@ -49,7 +42,7 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
  
  
          public ICollection<ISolrQuery> BuildFilterQueries(SearchParameters parameters) { 
-             var queriesFromFacets = from p in parameters.facets
+             var queriesFromFacets = from p in facetsInternal
                                      where p.value != null && p.value != ""
                                      select (ISolrQuery)Query.Field(p.name).Is(p.value); 
              return queriesFromFacets.ToList(); 
@@ -57,6 +50,7 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
 
 
         string[] AllFacetFields;
+        List<FacetInput.FacetMap> facetsInternal;
 
 
         public SearchResult Get([System.Web.Http.ModelBinding.ModelBinder(typeof(SM.General.Api.FieldValueModelBinder))]  SearchParameters parameters)
@@ -66,11 +60,23 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
                 
                 if (parameters == null)
                     parameters = new SearchParameters();
-                    
 
-                var AllFacetFieldsQuery = parameters.facets.GroupBy(f => f.name).Select(group => group.FirstOrDefault());
+                facetsInternal = new List<FacetInput.FacetMap>();
 
-                List<FacetInput> AllFacetFieldsList = AllFacetFieldsQuery.ToList();
+                foreach (var facetinput in parameters.facets) {
+
+                    if (facetinput.name == "type")
+                        facetsInternal.Add(new FacetInput.FacetMap("type", facetinput.value, facetinput.name));
+                    if(facetinput.name == "organization")
+                        facetsInternal.Add(new FacetInput.FacetMap("organization", UppercaseFirst(facetinput.value), facetinput.name));
+                    if (facetinput.name == "theme")
+                        facetsInternal.Add(new FacetInput.FacetMap("theme", facetinput.value, facetinput.name));
+                
+                }
+
+                var AllFacetFieldsQuery = facetsInternal.GroupBy(f => f.name).Select(group => group.FirstOrDefault());
+
+                List<FacetInput.FacetMap> AllFacetFieldsList = AllFacetFieldsQuery.ToList();
 
                 if (AllFacetFieldsList.Count > 0)
                 {
@@ -95,7 +101,7 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
              
                 var solr = ServiceLocator.Current.GetInstance<ISolrOperations<MetadataIndexDoc>>();
 
-                var start = parameters.offset - 1; //(parameters.offset - 1) * parameters.limit;
+                var start = parameters.offset-1; 
                 var metadataIndexDocs = solr.Query(BuildQuery(parameters), new QueryOptions
                 { 
                          FilterQueries = BuildFilterQueries(parameters),
@@ -107,7 +113,7 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
                                                      .ToList(),
                          }, 
                      });
-                return SearchResultOutput(metadataIndexDocs, parameters.limit, parameters.offset);
+                return SearchResultOutput(metadataIndexDocs, parameters.limit, parameters.offset, facetsInternal);
             }
             catch(Exception ex){
             
@@ -119,7 +125,7 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
         }
 
 
-        private static SearchResult SearchResultOutput(SolrQueryResults<MetadataIndexDoc> metadataIndexDocs, int Limit, int Offset)
+        private static SearchResult SearchResultOutput(SolrQueryResults<MetadataIndexDoc> metadataIndexDocs, int Limit, int Offset, List<FacetInput.FacetMap> facetsInternal)
         {
             List<Metadata> metadataList = new List<Metadata>(); // map'e fra metadataIndexDocs...
 
@@ -132,9 +138,9 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
                     Title = metadataIndexDoc.Title,
                     @Abstract = metadataIndexDoc.Abstract,
                     Type = metadataIndexDoc.Type,
-                    Theme = "TODO",
+                    Theme = metadataIndexDoc.Theme,
                     Organization = metadataIndexDoc.Organization,
-                    OrganizationLogo = "TODO",
+                    OrganizationLogo = metadataIndexDoc.OrganizationLogoUrl,
                     ThumbnailUrl = metadataIndexDoc.ThumbnailUrl,
                     DistributionUrl = metadataIndexDoc.DistributionUrl,
                     DistributionProtocol = metadataIndexDoc.DistributionProtocol,
@@ -152,6 +158,12 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
 
                 Facet facet = new Facet();
                 facet.FacetField = facetField.Key;
+
+                var mapedFacet = facetsInternal.FirstOrDefault(fi => fi.name == facetField.Key);
+                if (mapedFacet != null)
+                    facet.FacetField = mapedFacet.map;
+
+
                 facet.FacetResults = new List<Facet.FacetValue>();
 
                 foreach (var facetvalueFromIndex in metadataIndexDocs.FacetFields[facetField.Key])
@@ -176,6 +188,18 @@ namespace Kartverket.Metadatakatalog.Controllers.Api
                 Facets = facets
             };
             return SResult;
-        } 
+        }
+
+        static string UppercaseFirst(string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            // Return char and concat substring.
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+
     }
 }
--- Kartverket.Metadatakatalog/Models/Api/FacetInput.cs
+++ Kartverket.Metadatakatalog/Models/Api/FacetInput.cs
@@ -5,10 +5,30 @@ using System.Web;
 
 namespace Kartverket.Metadatakatalog.Models.Api
 {
+    
     public class FacetInput
     {
+        public FacetInput()
+        {
+        }
+        public FacetInput(string name,string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
         public string name { get; set; }
         public string value { get; set; }
 
+
+        public class FacetMap : FacetInput
+        {
+            public FacetMap(string name, string value, string map) : base (name, value)
+            {    
+            this.map = map;
+            }
+
+            public string map { get; set; }
+        
+        }
     }
 }
