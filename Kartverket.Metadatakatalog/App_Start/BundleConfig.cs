2c4e1a2e84eaa5cc9a51ce491130600800930a1a
Tor Kjetil <tor@arkitektum.no>
2014-12-11 15:00:22 +0100

    added new header and css

--- Kartverket.Metadatakatalog/App_Start/BundleConfig.cs
+++ Kartverket.Metadatakatalog/App_Start/BundleConfig.cs
@@ -9,8 +9,9 @@ namespace Kartverket.Metadatakatalog
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.typeahead.js"));
+                        "~/Scripts/jquery-{version}.js"
+                        ));
+            //"~/Scripts/jquery.typeahead.js"
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"
@@ -25,8 +26,9 @@ namespace Kartverket.Metadatakatalog
                       "~/Content/common.css",
                       "~/Content/navbar.css",
                       "~/Content/searchbar.css",
-                      "~/Content/jquery.typeahead.css",
+                     
                       "~/Content/site.css"));
+            // "~/Content/jquery.typeahead.css",
             //bundles.Add(new StyleBundle("~/Content/css").Include(
             //          "~/Content/bootstrap.css",
             //          "~/Content/geonorge-default.css",
