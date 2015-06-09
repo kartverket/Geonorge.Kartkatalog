213b758301322109efd8c23344bd45debd30d36f
Dag Olav <dagolav@arkitektum.no>
2015-06-09 11:50:48 +0200

    Config db

---
+++ Kartverket.Metadatakatalog/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Kartverket.Metadatakatalog.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Kartverket.Metadatakatalog.Models.MetadataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Kartverket.Metadatakatalog.Models.MetadataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ Kartverket.Metadatakatalog/Models/MetadataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Kartverket.Metadatakatalog.Models
+{
+    public class MetadataContext : DbContext
+    {
+        public MetadataContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
