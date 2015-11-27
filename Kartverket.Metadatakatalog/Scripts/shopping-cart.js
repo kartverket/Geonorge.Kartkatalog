752e531b299de7050e9b58c73f2d9648040e0466
Benjamin Dehli <benjamin@arkitektum.no>
2015-11-27 19:12:20 +0100

    Add javascript for shopping cart service

---
+++ Kartverket.Metadatakatalog/Scripts/shopping-cart.js
@@ -0,0 +1,34 @@
+﻿function setCookieValue(cookieName, cookieValue, cookieDomain) {
+    var now = new Date();
+    var hour = 4;
+    now.setTime(now.getTime() + (hour * 60 * 60 * 1000));
+    var cookieExpireDate = "expires=" + now.toGMTString();
+
+    console.log("cookieName: " + cookieName);
+    console.log("cookieValue: " + cookieValue);
+    console.log("cookieDomain: " + cookieDomain);
+    console.log("cookieExpireDate: " + cookieExpireDate);
+    document.cookie = "test=testverdi";
+    document.cookie = cookieName + "=" + cookieValue + "; " + cookieExpireDate + "; domain=." + cookieDomain + ";path=/";
+}
+
+function deleteCookieValue(cookieName) {
+    var d = new Date();
+    d.setTime(d.getTime());
+    var cookieExpireDate = "expires=" + d.toString();
+    document.cookie = cookieName + "=expired;" + cookieExpireDate;
+}
+
+var shoppingCartElement = $('.shopping-cart-container .shopping-cart #orderitem-count');
+
+$(window).load(function () {
+    var orderItems = JSON.parse(localStorage.getItem("orderItems"));
+    var cookieName = "orderitems";
+    var cookieValue = orderItems.length;
+    var cookieDomain = "geonorge.no";
+      
+    $.cookie(cookieName, cookieValue, { expires: 7 });
+
+    //deleteCookieValue(cookieName);
+   // setCookieValue(cookieName, cookieValue, cookieDomain);
+});
