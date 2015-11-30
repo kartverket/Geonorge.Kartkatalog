e3940af803bcf1dd0a9eb4b2bc6ff60eae5ac4bf
Benjamin Dehli <benjamin@arkitektum.no>
2015-11-30 19:23:05 +0100

    angular bug fix test 2

--- Kartverket.Metadatakatalog/KartverketSharedMenu/Scripts/geonorge/app.js
+++ Kartverket.Metadatakatalog/KartverketSharedMenu/Scripts/geonorge/app.js
@@ -1,10 +1,5 @@
 ﻿angular.module('geonorge', ['ui.bootstrap']);
 
-var app = angular.module('geonorge', ['ui.bootstrap']);
-app.controller('MainCtrl', function ($scope, $sce) {
-    $scope.trustSrc = function (src) {
-        return $sce.trustAsResourceUrl(src);
-    }
-
-    $scope.url = { src: "http://kartkatalog.dev.geonorge.no/KartverketSharedMenu/Scripts/geonorge/partials/_autoCompleteRow.html" };
+angular.module('geonorge').config(function ($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist(['**']);
 });
