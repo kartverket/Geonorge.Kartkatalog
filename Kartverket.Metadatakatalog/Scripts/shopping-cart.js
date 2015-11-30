18969fc32ae1ac896cddeac9b5a637e692c49b11
Benjamin Dehli <benjamin@arkitektum.no>
2015-11-30 15:43:19 +0100

    Add value from cookie to shopping cart icon

--- Kartverket.Metadatakatalog/Scripts/shopping-cart.js
+++ Kartverket.Metadatakatalog/Scripts/shopping-cart.js
@@ -1,24 +1,31 @@
 ﻿function updateShoppingCart() {
     var shoppingCartElement = $('#orderitem-count');
     var orderItems = "";
-    if (localStorage.getItem("orderItems") != null && localStorage.getItem("orderItems") != "[]") {
-        orderItems = localStorage.getItem("orderItems");
-    } else {
-        orderItems = "";
-    }
     var orderItemsObj = {};
     var cookieName = "orderitems";
     var cookieValue = 0;
     var cookieDomain = ".geonorge.no";
 
-    if (orderItems == "") {
-        shoppingCartElement.css("display", "none");
+
+    if (localStorage.getItem("orderItems") != null && localStorage.getItem("orderItems") != "[]") {
+        orderItems = localStorage.getItem("orderItems");
     } else {
+        orderItems = "";
+    }
+
+    if (orderItems != "") {
         shoppingCartElement.css("display", "block");
         orderItemsObj = JSON.parse(orderItems);
         cookieValue = orderItemsObj.length;
         shoppingCartElement.html(cookieValue);
-    } 
+
+    } else if ($.cookie(cookieName)) {
+        cookieValue = $.cookie(cookieName);
+        shoppingCartElement.css("display", "block");
+        shoppingCartElement.html(cookieValue);
+    } else {
+        shoppingCartElement.css("display", "none");
+    }
     $.cookie(cookieName, cookieValue, { expires: 7, path: '/', domain: cookieDomain });
 }
 
