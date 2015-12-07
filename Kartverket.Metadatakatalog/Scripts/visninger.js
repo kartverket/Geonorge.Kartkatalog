1901b020684912fde0d10bb9a43493354373b00a
Benjamin Dehli <benjamin@arkitektum.no>
2015-12-07 18:36:43 +0100

    Add order function for title and organization in table

--- Kartverket.Metadatakatalog/Scripts/visninger.js
+++ Kartverket.Metadatakatalog/Scripts/visninger.js
@@ -1,4 +1,8 @@
-﻿function changeLayout(layout) {
+﻿function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null
+}
+
+function changeLayout(layout) {
     $(".search-results").attr("id", layout);
     var options = $("#layoutSelect option");
     var selectedOption = $("#layoutSelect option[value='" + layout + "']");
@@ -12,11 +16,39 @@
     localStorage.setItem("layout", layout);
 }
 
+
+function orderBy() {
+    var orderbyArray = getURLParameter("orderby").split("_");
+    var orderby = orderbyArray[0];
+    var direction = "asc";
+    if (orderbyArray.length > 1) {
+        direction = orderbyArray[1];
+    }
+    var orderHeaders = $("#tableView .search-results-table-heading .orderby");
+
+    $.each(orderHeaders, function () {
+        if ($(this).hasClass("orderby-" + orderby)) {
+            if ($(this).hasClass("orderby-" + direction)) {
+                $(this).hide();
+            } else {
+                $(this).show();
+                $(this).addClass("active-orderby");
+            }
+        } else if ($(this).hasClass("orderby-asc")) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
+    });
+}
+
+
 $(document).ready(function () {
     var layout = "tableView";
     if (localStorage.getItem("layout") != null) {
         layout = localStorage.getItem("layout");
     };
+    orderBy();
     changeLayout(layout);
 });
 
@@ -27,13 +59,15 @@ function additionalView(buttonId) {
 }
 
 
+
+
+/*
 function SortBy(sort) {
     var sort = document.getElementById("sorting");
     var selected = sort.options[sort.selectedIndex].text;
     localStorage.setItem("sortering", selected);
     document.sortering.submit();
-
-}
+}*/
 
 
 // Loading animation
