878a0b331632689eab898664b99e35c4bdff28bd
Henning Jensen <henning.jensen@gmail.com>
2016-02-09 20:53:31 +0100

    Bundle scripts and styles into one bundle.

--- Kartverket.Metadatakatalog/App_Start/BundleConfig.cs
+++ Kartverket.Metadatakatalog/App_Start/BundleConfig.cs
@@ -9,6 +9,66 @@ namespace Kartverket.Metadatakatalog
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //***************** complete bundle - remember to update this as well as the separate bundles ****************//
+
+            bundles.Add(new ScriptBundle("~/bundles/kartkatalog-scripts").Include(
+            "~/KartverketSharedMenu/Scripts/angular-1.4.9.min.js",
+            "~/KartverketSharedMenu/Scripts/jquery-1.11.3.js",
+            "~/KartverketSharedMenu/Scripts/geonorge/app.js",
+            "~/KartverketSharedMenu/Scripts/geonorge-top/searchOptions/" + ConfigurationManager.AppSettings["SearchOptionsFile"],
+            "~/KartverketSharedMenu/Scripts/geonorge-top/searchTopController.js",
+            "~/KartverketSharedMenu/Scripts/ui-bootstrap-1.1.0.js",
+            "~/KartverketSharedMenu/Scripts/geonorge-common/baseUrl.js",
+            "~/KartverketSharedMenu/Scripts/geonorge-common/common.js",
+            "~/KartverketSharedMenu/Scripts/geonorge-top/menuTopController.js",
+            "~/Scripts/bootstrap.js",
+            "~/Scripts/site.js",
+            "~/Scripts/jquery-ui-datepicker.js",
+            "~/Scripts/respond.js",
+            "~/Scripts/jquery.cookie.js",
+            "~/Scripts/jquery.validate*",
+            "~/Scripts/modernizr-*",
+            "~/Scripts/chosen.jquery.js",
+            "~/Scripts/easyXDM.js",
+            "~/Scripts/visninger.js",
+            "~/Scripts/init.js",
+            "~/Scripts/shopping-cart.js"
+            ));
+
+
+            bundles.Add(new StyleBundle("~/bundles/kartkatalog-styles").Include(
+                "~/KartverketSharedMenu/Styles/bootstrap.css",
+                "~/KartverketSharedMenu/Styles/geonorge-top/menuTop.css",
+                "~/KartverketSharedMenu/Styles/geonorge-top/logoTop.css",
+                "~/KartverketSharedMenu/Styles/geonorge-top/searchTop.css",
+                "~/Content/site.css",
+                "~/Content/jquery-ui.css",
+                "~/Content/jquery-ui.structure.css",
+                "~/Content/jquery-ui.theme.css",
+                "~/Content/themes/base/core.css",
+                "~/Content/themes/base/datepicker.css",
+                "~/Content/themes/base/theme.css",
+                "~/Content/custom.css",
+                "~/Content/ie-10-only.css",
+                "~/Content/ie-11-only.css",
+                "~/Content/geonorge-colors.css",
+                "~/Content/searchresult-layout.css",
+                "~/Content/custom-icons.css",
+                "~/Content/font-awesome.min.css",
+                "~/Content/forms.css",
+                "~/Content/chosen.css",
+                "~/Content/download.css",
+                "~/Content/map-modal.css",
+                "~/Content/shopping-cart.css"
+           ));
+
+
+            
+            
+
+
+
+            //***************** separate bundles - used in other applications ****************//
 
             bundles.Add(new ScriptBundle("~/bundles/shared-menu-scripts").Include(
             "~/KartverketSharedMenu/Scripts/angular-1.4.9.min.js",
