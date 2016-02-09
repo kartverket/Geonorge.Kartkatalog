5b3699e813531091718975f704cdf08b3bcf8b59
Henning Jensen <henning.jensen@gmail.com>
2016-02-09 20:37:54 +0100

    Add compression and whitespace filter.

--- Kartverket.Metadatakatalog/App_Start/FilterConfig.cs
+++ Kartverket.Metadatakatalog/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using Kartverket.Metadatakatalog.ActionFilters;
 
 namespace Kartverket.Metadatakatalog
 {
@@ -8,6 +9,7 @@ namespace Kartverket.Metadatakatalog
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new WhitespaceFilter());
         }
     }
 }
