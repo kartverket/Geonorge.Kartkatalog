9aaa7944bcd0b159d8187d0b7759d3c50026c088
Benjamin Dehli <benjamin@arkitektum.no>
2016-03-22 08:13:35 +0100

    Update bower components

--- Kartverket.Metadatakatalog/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
+++ Kartverket.Metadatakatalog/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js
@@ -1,3 +1,25 @@
+var applicationEnvironment = (applicationEnvironment === undefined) ? "" : applicationEnvironment;
+var geonorgeUrl = (applicationEnvironment === "") ? "https://www.test.geonorge.no/" : "https://www.geonorge.no/";
+
+$(window).load(function () {
+    var options = {
+        disable_search_threshold: 10,
+        search_contains: true
+    };
+    $(".chosen-select").chosen(options);
+    $('[data-toggle="tooltip"]').tooltip();
+
+    // Get useragent
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+});
+
+$("document").ready( function(){
+	if ($("#geonorge-logo").length){ 
+		$("#geonorge-logo a").prop("href", geonorgeUrl);
+		$("#geonorge-logo a img").prop("src", "/Content/bower_components/kartverket-felleskomponenter/assets/images/geonorge_" + applicationEnvironment + "logo.svg");
+	}    
+});
 angular.module('geonorge', ['ui.bootstrap']);
 
 angular.module('geonorge').config(["$sceDelegateProvider", function ($sceDelegateProvider) {
@@ -592,18 +614,6 @@ $(document).ready(function () {
       }]);
 }());
 
-$(window).load(function () {
-    var options = {
-        disable_search_threshold: 10,
-        search_contains: true
-    };
-    $(".chosen-select").chosen(options);
-    $('[data-toggle="tooltip"]').tooltip();
-
-    // Get useragent
-    var doc = document.documentElement;
-    doc.setAttribute('data-useragent', navigator.userAgent);
-});
 function updateShoppingCart() {
     var shoppingCartElement = $('#orderitem-count');
     var orderItems = "";
