cae7811cdfe916266ab92d1260b0232b030af39c
Benjamin Dehli <benjamin@arkitektum.no>
2017-01-30 13:52:44 +0100

    Add javascript logic for multiple selection of facets in same category

--- Kartverket.Metadatakatalog/App_Start/BundleConfig.cs
+++ Kartverket.Metadatakatalog/App_Start/BundleConfig.cs
@@ -65,7 +65,8 @@ namespace Kartverket.Metadatakatalog
             bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
                 "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
                 "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendorfonts.min.css",
-                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css",
+                "~/Content/temp.css"
                 ));
 
             bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/js/scripts").Include(
--- Kartverket.Metadatakatalog/Scripts/visninger.js
+++ Kartverket.Metadatakatalog/Scripts/visninger.js
@@ -6,6 +6,67 @@
     }
 }
 
+
+function getAllUrlParams(url) {
+
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+    // we'll store the parameters here
+    var obj = {};
+
+    // if query string exists
+    if (queryString) {
+
+        // stuff after # is not part of query string, so get rid of it
+        queryString = queryString.split('#')[0];
+
+        // split our query string into its component parts
+        var arr = queryString.split('&');
+
+        for (var i = 0; i < arr.length; i++) {
+            // separate the keys and the values
+            var a = arr[i].split('=');
+
+            // in case params look like: list[]=thing1&list[]=thing2
+            var paramNum = undefined;
+            var paramName = a[0].replace(/\[\d*\]/, function (v) {
+                paramNum = v.slice(1, -1);
+                return '';
+            });
+
+            // set parameter value (use 'true' if empty)
+            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+
+            // if parameter name already exists
+            if (obj[paramName]) {
+                // convert value to array (if still string)
+                if (typeof obj[paramName] === 'string') {
+                    obj[paramName] = [obj[paramName]];
+                }
+                // if no array index number specified...
+                if (typeof paramNum === 'undefined') {
+                    // put the value on the end of the array
+                    obj[paramName].push(paramValue);
+                }
+                    // if array index number specified...
+                else {
+                    // put the value at that index number
+                    obj[paramName][paramNum] = paramValue;
+                }
+            }
+                // if param name doesn't exist yet, set it
+            else {
+                obj[paramName] = paramValue;
+            }
+        }
+    }
+
+    return obj;
+}
+
+
 function changeLayout(layout) {
     $(".search-results").attr("id", layout);
     var options = $("#layoutSelect option");
@@ -71,7 +132,84 @@ function getFacetFromUrl() {
     }
     var activeFiltersJSON = JSON.stringify(activeFilters);
     localStorage.setItem("activeFilters", activeFiltersJSON);
+
 };
+
+function createFacetUrlParameter(index, name, value, trailingAmpersand) {
+    var ampersand = '';
+    if (trailingAmpersand !== undefined && trailingAmpersand == true) ampersand = '&';
+
+    var facetUrlParameter = 'Facets%5B' + index + '%5D.name=' + name + '&Facets%5B' + index + '%5D.value=' + value + ampersand;
+
+    return facetUrlParameter;
+}
+
+function getSelectedFacets() {
+    var allUrlParameters = getAllUrlParams(location.search);
+    var allFacets = {};
+    // get all facets
+    for (var parameter in allUrlParameters) {
+        if (allUrlParameters.hasOwnProperty(parameter)) {
+            var res = parameter.split(".");
+            var facet = allFacets[res[0]] !== undefined ? allFacets[res[0]] : {};
+            facet[res[1]] = allUrlParameters[parameter];
+            allFacets[res[0]] = facet;
+
+        }
+    }
+    return allFacets;
+}
+
+function getAllUrlParamsForSelectedFacets(selectedFacets) {
+    var allFacets = selectedFacets !== undefined ? selectedFacets : getSelectedFacets();
+    var allUrlParameters = { parameterString: '?', parameterLength: 0 };
+    var facetIndex = 0;
+    for (var key in allFacets) {
+        if (allFacets.hasOwnProperty(key)) {
+            var facet = allFacets[key];
+            console.log("facet");
+            console.log(facet);
+            var urlParameter = '';
+            if (facet.value !== undefined) {
+                urlParameter = createFacetUrlParameter(allUrlParameters.parameterLength, facet.name, facet.value, true);
+                console.log("urlParameter");
+                console.log(urlParameter);
+                allUrlParameters.parameterString += urlParameter;
+                allUrlParameters.parameterLength++;
+            }
+        }
+    }
+
+    return allUrlParameters;
+}
+
+function getUrlForFacetLink(facetLinkElement) {
+    var facetName = $(facetLinkElement).data('facet-name');
+    var facetValue = $(facetLinkElement).data('facet-value');
+    var selectedFacets = getSelectedFacets();
+    var alreadySelected = false;
+
+    for (var key in selectedFacets) {
+        if (selectedFacets.hasOwnProperty(key)) {
+            var facet = selectedFacets[key];
+            // Remove already selected facet
+            if (facet.name == facetName && facet.value == facetValue) {
+                delete selectedFacets[key];
+                alreadySelected = true;
+            }
+        }
+    }
+
+    var newUrlParameter = '';
+    var urlParameters = getAllUrlParamsForSelectedFacets(selectedFacets);
+    if (!alreadySelected) newUrlParameter = createFacetUrlParameter(urlParameters.parameterLength, facetName, facetValue, false);
+    else urlParameters.parameterString = urlParameters.parameterString.slice(0, -1); // Remove last amperstand
+
+    var mergedUrlParameters = urlParameters.parameterString + newUrlParameter;
+
+    return mergedUrlParameters;
+}
+
 
 $(document).ready(function () {
     var layout = "tableView";
@@ -80,6 +218,13 @@ $(document).ready(function () {
     };
     orderBy();
     changeLayout(layout);
+
+    $('.facet-link').on('click', function (event) {
+        event.preventDefault();
+        var facetLinkUrl = getUrlForFacetLink($(this));
+
+        window.location.href = facetLinkUrl;
+    })
 });
 
 
