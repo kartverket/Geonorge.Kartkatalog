996c8e40a13b8a06a7078189e2ed1b8a3e478b38
Benjamin Dehli <benjamin@arkitektum.no>
2017-02-07 15:01:42 +0100

    Remove unused script code

--- Kartverket.Metadatakatalog/Scripts/visninger.js
+++ Kartverket.Metadatakatalog/Scripts/visninger.js
@@ -103,114 +103,7 @@ function orderBy() {
     }
 }
 
-function getFacetFromUrl() {
-    var activeFilters = {}
-    if (JSON.parse(localStorage.getItem("activeFilters")) != null) {
-        activeFilters = JSON.parse(localStorage.getItem("activeFilters"));
-    };
-    var regExp = new RegExp(".*Facets\\[0\\]\.name=([a-z]*)&.*");
-    var searchString = decodeURIComponent(location.search);
-    var facetName = regExp.exec(searchString);
-    if (facetName != null) {
-        $.each(facetName, function (key, value) {
-            if (value == 'theme') {
-                activeFilters['#theme'] = true;
-            }
-        });
-    }
-    var activeFiltersJSON = JSON.stringify(activeFilters);
-    localStorage.setItem("activeFilters", activeFiltersJSON);
 
-};
-
-function createFacetUrlParameter(index, name, value, trailingAmpersand) {
-    var ampersand = '';
-    if (trailingAmpersand !== undefined && trailingAmpersand == true) ampersand = '&';
-
-    var facetUrlParameter = 'Facets%5B' + index + '%5D.name=' + name + '&Facets%5B' + index + '%5D.value=' + value + ampersand;
-
-    return facetUrlParameter;
-}
-
-function createStringUrlParameter(name, value, trailingAmpersand) {
-    var ampersand = '';
-    if (trailingAmpersand !== undefined && trailingAmpersand == true) ampersand = '&';
-
-    var stringUrlParameter = name + '=' + value + ampersand;
-
-    return stringUrlParameter;
-
-}
-
-function getSelectedFacets() {
-    var allUrlParameters = getAllUrlParams(location.search);
-    var allFacets = {};
-    // get all facets
-    for (var parameter in allUrlParameters) {
-        if (allUrlParameters.hasOwnProperty(parameter)) {
-            var res = parameter.split(".");
-            var facet = allUrlParameters[parameter];
-            if (res.length > 1) {
-                var facet = allFacets[res[0]] !== undefined ? allFacets[res[0]] : {};
-                facet[res[1]] = allUrlParameters[parameter];
-            }
-            allFacets[res[0]] = facet;
-
-        }
-    }
-    return allFacets;
-}
-
-function getAllUrlParamsForSelectedFacets(selectedFacets) {
-    var allFacets = selectedFacets !== undefined ? selectedFacets : getSelectedFacets();
-    var allUrlParameters = { parameterString: '?', parameterLength: 0 };
-    var facetIndex = 0;
-    for (var key in allFacets) {
-        if (allFacets.hasOwnProperty(key)) {
-            var facet = allFacets[key];
-            var urlParameter = '';
-            if (typeof (facet) == "string") {
-                urlParameter = createStringUrlParameter(key, facet, true);
-                allUrlParameters.parameterString += urlParameter;
-            } else if (facet.value !== undefined) {
-                urlParameter = createFacetUrlParameter(allUrlParameters.parameterLength, facet.name, facet.value, true);
-                allUrlParameters.parameterString += urlParameter;
-                allUrlParameters.parameterLength++;
-            }
-        }
-    }
-
-    return allUrlParameters;
-}
-
-function getUrlForFacetLink(facetLinkElement) {
-    var facetName = $(facetLinkElement).data('facet-name');
-    var facetValue = $(facetLinkElement).data('facet-value');
-    var selectedFacets = getSelectedFacets();
-    var alreadySelected = false;
-
-    for (var key in selectedFacets) {
-        if (selectedFacets.hasOwnProperty(key)) {
-            var facet = selectedFacets[key];
-            // Remove already selected facet
-            if (facet.name == escape(facetName) && facet.value == escape(facetValue)) {
-                delete selectedFacets[key];
-                alreadySelected = true;
-            }
-        }
-    }
-
-    var newUrlParameter = '';
-    var urlParameters = getAllUrlParamsForSelectedFacets(selectedFacets);
-    if (!alreadySelected) newUrlParameter = createFacetUrlParameter(urlParameters.parameterLength, facetName, facetValue, false);
-    else urlParameters.parameterString = urlParameters.parameterString.slice(0, -1); // Remove last amperstand
-
-    var mergedUrlParameters = urlParameters.parameterString + newUrlParameter;
-
-    var url = window.location.origin + window.location.pathname + mergedUrlParameters;
-
-    return url;
-}
 
 
 $(document).ready(function () {
@@ -220,13 +113,6 @@ $(document).ready(function () {
     };
     orderBy();
     changeLayout(layout);
-
-    $('.facet-link').on('click', function (event) {
-        event.preventDefault();
-        var facetLinkUrl = getUrlForFacetLink($(this));
-
-        window.location.href = facetLinkUrl;
-    })
 });
 
 
