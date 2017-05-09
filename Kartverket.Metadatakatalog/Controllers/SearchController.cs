d64de673c49b0459403202b39c65094f03835d36
veronika@arkitektum.no <veronika@arkitektum.no>
2017-05-09 08:03:48 +0200

    Add and remove test code

--- Kartverket.Metadatakatalog/Controllers/SearchController.cs
+++ Kartverket.Metadatakatalog/Controllers/SearchController.cs
@@ -53,6 +53,19 @@ namespace Kartverket.Metadatakatalog.Controllers
             return View(model);
         }
 
+        public ActionResult AreaNy(SearchByAreaParameters parameters)
+        {
+            parameters.AddDefaultFacetsIfMissing();
+            parameters.CreateFacetOfArea();
+            parameters.AddComplexFacetsIfMissing();
+            FixAreaParameters(parameters);
+
+            var searchResult = _searchService.Search(parameters);
+            var model = new SearchByAreaViewModel(parameters, searchResult);
+
+            return View(model);
+        }
+
         protected override void OnException(ExceptionContext filterContext)
         {
             Log.Error("Error", filterContext.Exception);
