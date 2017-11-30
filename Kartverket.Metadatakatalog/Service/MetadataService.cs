80439c6e60702075c3407224f12d18b21a732a96
Dag Olav Dahle <dagolav@arkitektum.no>
2017-11-30 08:59:23 +0100

    Formatting

--- Kartverket.Metadatakatalog/Service/MetadataService.cs
+++ Kartverket.Metadatakatalog/Service/MetadataService.cs
@@ -296,7 +296,7 @@ namespace Kartverket.Metadatakatalog.Service
                         tmp.DistributionUrl = relData[7] ?? "";
                         tmp.Protocol = relData[6] != null ? Register.GetDistributionType(relData[6]) : "";
                         if (tmp.Type == "servicelayer")
-                            tmp.Protocol = tmp.Protocol + "-lag";
+                            tmp.Protocol = tmp.Protocol + "lag";
                         tmp.Organization = relData[4];
                         tmp.ShowDetailsUrl = "/metadata/org/title/" + tmp.Uuid;
 
