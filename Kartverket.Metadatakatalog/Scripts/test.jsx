14e200660096ecb59ecad44c34ba0d7a7313080b
Benjamin Dehli <benjamin@arkitektum.no>
2018-11-19 15:16:54 +0100

    wip

---
+++ Kartverket.Metadatakatalog/Scripts/test.jsx
@@ -0,0 +1,39 @@
+﻿class Test extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        };
+    }
+    getMapItems() {
+        if (localStorage.mapItems) {
+            return JSON.parse(localStorage.mapItems);
+        } else {
+            return null;
+        }
+    }
+    renderMapItemList() {
+        var mapItems = this.getMapItems();
+        if (mapItems) {
+            let mapItemElements = this.getMapItems().map(function (mapItem, i) {
+                return <li key={i}>{mapItem.name}</li>;
+            });
+            let mapListElement = React.createElement('ul', { className: 'mapItemList' }, mapItemElements);
+            return mapListElement;
+        }
+    }
+    render() {
+        return (
+            <div className={this.state.active ? 'map' : 'hidden'}>
+                <div className="custom-modal">
+                    <div className="custom-modal-container">
+                        { this.renderMapItemList() }
+                        <iframe style={{ height: '100%' }} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Test />, document.getElementById('react-content'));
